576e91d3c43564daa6686eb738a8e5f3714dcea0
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-10-03 12:24:38 -0700

    refactored ExecutePassButton.js to use GameContext for shared state

--- src/components/ExecutePassButton.js
+++ src/components/ExecutePassButton.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GameContext } from '../Context/GameContext';
 import Card from './Card';
 
 export default function ExecutePassButton({
   passCard,
-  setFrom,
   from,
-  to,
-  selectedCard,
-  setSelectedCard,
+  to
 }) {
+
+  const { selectedCard, setSelectedCard, setFrom } = useContext(GameContext);
+
   return (
     <div className="execute-button" onClick={() => passCard(selectedCard)}>
       Pass <Card card={selectedCard} setSelectedCard={setSelectedCard} setFrom={setFrom} /> from{' '}
